10416ec7551413813e1b7db0fe5b36b344be29b6
lewisharris <44181381+lewisharris@users.noreply.github.com>
2019-03-03 22:33:29 +0000

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -16,3 +16,30 @@ container.addEventListener('mouseout',function(){
 		container.className = 'clickableContainer';
 		caption.className = 'caption';
 });
+
+var hamburger = document.getElementsByClassName('hamburgerContainer')[0];
+var menuOverlay = document.getElementById('menuoverlay');
+var overlay = document.getElementById('overlay');
+
+
+hamburger.addEventListener('click',function(){
+	if(hamburger.className == 'hamburgerContainer'){
+	hamburger.className = 'hamburgerContainerHover';
+	menuOverlay.className = "open";
+	}
+	else{
+		hamburger.className = 'hamburgerContainer';
+		menuOverlay.className = "";
+
+	}
+});
+
+
+
+
+
+overlay.addEventListener('click',function(){
+	hamburger.className = 'hamburgerContainer';
+
+	}
+);
